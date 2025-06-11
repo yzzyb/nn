7e622f394590f1fb9dd05c0d4d6625234b5cc311
张博涵 <99076201+SavoreLotty@users.noreply.github.com>
2025-06-11 18:16:46 +0800

    CNN_pytorch补充注释 (#1714)

--- src/chap05_CNN/CNN_pytorch.py
+++ src/chap05_CNN/CNN_pytorch.py
@@ -9,7 +9,7 @@ import torch.utils.data as Data
 import torchvision               # 包含常用的数据集和模型
 import torch.nn.functional as F  # 包含常用的函数式API，如ReLU, softmax等
 import numpy as np
-from torch.autograd import Variable
+from torch.autograd import Variable # 导入自动微分变量，用于构建计算图和自动求导
 
 # 设置超参数
 learning_rate = 1e-4  #  学习率
