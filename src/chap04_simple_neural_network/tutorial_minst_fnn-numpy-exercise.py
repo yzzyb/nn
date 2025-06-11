52b08e8276297034796eb9ccf17acf549b71bef4
xia-yiqing <2474951437@qq.com>
2025-06-11 21:48:11 +0800

    Update tutorial_minst_fnn-numpy-exercise.py (#1871)

--- src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
+++ src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
@@ -294,6 +294,7 @@ with tf.GradientTape() as tape:
     x, W1, W2, label = tf.constant(x), tf.constant(W1), tf.constant(W2), tf.constant(label)
     tape.watch(W1)        # 追踪 W1 的梯度
     tape.watch(W2)        # 追踪 W2 的梯度
+    # 第一层线性变换：输入x与权重W1做矩阵乘法
     h1 = tf.matmul(x, W1)
     h1_relu = tf.nn.relu(h1)
     h2 = tf.matmul(h1_relu, W2)
