19401968cd4237865427cb8276cdf407f8a76324
boyu111 <1980019141@qq.com>
2025-06-13 21:22:08 +0800

    添加注释

--- src/chap11_gaussian_mixture/GMM.py
+++ src/chap11_gaussian_mixture/GMM.py
@@ -262,7 +262,7 @@ class GaussianMixtureModel:
             # 处理非奇异协方差矩阵
             inv = np.linalg.inv(sigma) #计算协方差矩阵的逆
             exponent = -0.5 * np.einsum('...i,...i->...', X_centered @ inv, X_centered) #计算指数部分（二次型）
-            return -0.5 * n_features * np.log(2 * np.pi) - 0.5 * logdet + exponent
+            return -0.5 * n_features * np.log(2 * np.pi) - 0.5 * logdet + exponent #组合对数概率密度
         
     def plot_convergence(self):
         """可视化对数似然的收敛过程"""
