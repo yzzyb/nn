9eacf8c8af5bd30a38fc0967d7c3b5b6ab4c739d
qqqqq-stack <1349929289@qq.com>
2025-06-13 12:48:10 +0800

    Update rbm.py (#2106)

--- src/chap12_RBM/rbm.py
+++ src/chap12_RBM/rbm.py
@@ -213,7 +213,7 @@ if __name__ == '__main__':
     except IOError:
         # 如果文件不存在或加载失败，生成新的二值化MNIST数据
         (train_images, _), (_, _) = mnist.load_data() # 加载MNIST数据
-        mnist_bin = (train_images >= 128).astype(np.int8) # 二值化处理
+        mnist_bin = (train_images >= 128).astype(np.uint8) # 二值化处理
         np.save('mnist_bin.npy', mnist_bin) # 保存为.npy文件
         # 重新加载刚生成的文件
         mnist = np.load('mnist_bin.npy')
