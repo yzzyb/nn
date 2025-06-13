fa7574e658a624a2270ef26d7598eb2459a64ee5
cxchenxi666 <11525668@qq.com>
2025-06-13 10:35:18 +0800

    补充注释 (#2055)

--- src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
+++ src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
@@ -257,6 +257,7 @@ class Log:
 
 x = np.random.normal(size=[5, 6])   # 示例：生成 5 个样本，每个样本 6 维特征
 label = np.zeros_like(x)            # 创建了一个与 x 形状相同的全零标签矩阵
+# 手动设置每个样本的类别标签
 label[0, 1] = 1.
 label[1, 0] = 1
 label[2, 3] = 1
